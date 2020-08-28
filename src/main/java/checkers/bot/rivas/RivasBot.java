350fa067e98ce0dc5e5467ba27bd49f24c9e4418
pablin2402 <pabloperedo04@gmail.com>
2020-08-28 16:17:39 -0400

    add package rivas and RivasBot class created

---
+++ src/main/java/checkers/bot/rivas/RivasBot.java
@@ -0,0 +1,12 @@
+package checkers.bot.rivas;
+
+import checkers.CheckersBoard;
+import checkers.CheckersMove;
+import checkers.CheckersPlayer;
+
+public class RivasBot implements CheckersPlayer {
+    @Override
+    public CheckersMove play(CheckersBoard board) {
+        return null;
+    }
+}
