7a66ed218df205d4b8390d17f6f123ed62d4295f
pablin2402 <pabloperedo04@gmail.com>
2020-09-01 22:48:45 -0400

    Changes in CheckersPablo, bug fixed but implementation is 70%

--- src/main/java/checkers/bot/rivas/CheckersPablo.java
+++ src/main/java/checkers/bot/rivas/CheckersPablo.java
@@ -1,30 +1,137 @@
-package checkers.bot.rivas;
+    package checkers.bot.rivas;
 
 import checkers.CheckersBoard;
 import checkers.CheckersMove;
+import checkers.exception.BadMoveException;
 
 import java.util.*;
 
 public class CheckersPablo extends CheckersBoard {
-    List<CheckersMove> children ;
-    public int heuristic() {
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        CheckersPablo that = (CheckersPablo) o;
+        return Objects.equals(successors, that.successors);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), successors);
+    }
+
+    public int heuristic(CheckersBoard b) {
         int result = 0;
-        System.out.println("caca"+ getBoard()[1][1]);
+        char[][] board = b.getBoard();
         for ( int i = 0; i < 8; i++ ) {
             for ( int j = 0; j < 8; j++ ) {
-                if ( getBoard()[ i ][ j ] == 'b' ) {
+                if ( board[ i ][ j ] == BLACK_PLAIN ) {
                     result += 2;
-                } else if ( getBoard()[ i ][ j ] == 'r' ) {
+                } else if ( board[ i ][ j ] == RED_PLAIN ) {
                     result -= 2;
-                } else if (getBoard()[ i ][ j ] == Character.toUpperCase( 'b' ) ) {
+                } else if (board[ i ][ j ] == BLACK_CROWNED) {
                     result += 5;
-                } else if (getBoard()[ i ][ j ] == Character.toUpperCase( 'r' ) ) {
+                } else if (board[ i ][ j ] == RED_CROWNED) {
                     result -= 5;
                 }
             }
         }
         return result;
     }
+    List<CheckersMove> successors = new ArrayList<>();
+
+    protected CheckersMove getBestMove(CheckersBoard board, boolean myPlayer, int level)  {
+        CheckersBoard.Player startingPlayer;
+        startingPlayer = CheckersBoard.initBoard().getCurrentPlayer();
+
+        generatePossibleMoves(board);
+        CheckersBoard child = null;
 
 
+        List<Integer> heuristics = new ArrayList<>(100);
+        if (successors.isEmpty()){
+            return  null;
+        }
+        for (CheckersMove successor : successors) {
+            child = board.clone();
+            try {
+                child.processMove(successor);
+            } catch (BadMoveException ex) {
+                System.err.println(ex.getMessage());
+
+            }
+            heuristics.add(getUtility(level - 1, otherPlayer(startingPlayer), child, !myPlayer));
+        }
+        CheckersMove checkersMove = null;
+        int maxUtility = Integer.MIN_VALUE;
+        for (Integer heuristic : heuristics) {
+            if (heuristic > maxUtility) {
+                maxUtility = heuristic;
+
+            }
+        }
+        System.out.println(maxUtility);
+
+        System.out.println("ANTES");
+        for (int i=0; i< successors.size();i++){
+            System.out.println(":"+successors.get(i).getStartCol());
+        }
+        for (int i = 0; i < heuristics.size(); i++) {
+            if (heuristics.get(i) < maxUtility) {
+                    heuristics.remove(i);
+                    successors.remove(i);
+                    i--;
+            }
+        }
+        System.out.println("DESPUES");
+        for (int i=0; i<successors.size();i++){
+            System.out.println(":"+successors.get(i).getStartCol());
+        }
+        Random rand = new Random();
+        return successors.get(rand.nextInt(successors.size()));
+    }
+
+    private void generatePossibleMoves(CheckersBoard board) {
+        List<CheckersMove> possibleCaptures = board.possibleCaptures();
+        if (possibleCaptures.isEmpty()) {
+            successors= board.possibleMoves();
+        }else{
+            successors = board.possibleCaptures();
+        }
+    }
+
+    private int getUtility(int level, CheckersBoard.Player player, CheckersBoard board, boolean myPlayer) {
+        if (possibleMoves().isEmpty()){
+            return heuristic(board);
+        }
+        if (level == 0) {
+            return heuristic(board);
+        }
+        List<CheckersMove> possibleMoves = board.possibleMoves();
+        CheckersBoard checkersBoard = null;
+        if (myPlayer) {
+            int maxUtility= Integer.MIN_VALUE;
+            for(int i=0; i<possibleMoves.size(); i++){
+                checkersBoard= board.clone();
+                int result = getUtility(level-1, otherPlayer(player),checkersBoard, !myPlayer);
+                if(result >maxUtility){
+                    maxUtility = result;
+                }
+            }
+            System.out.println("MAX UTILITY"+maxUtility);
+            return maxUtility;
+        }else {
+            int minUtility= Integer.MAX_VALUE;
+            for(int i=0; i<possibleMoves.size(); i++){
+                checkersBoard= board.clone();
+                int result = getUtility(level-1, otherPlayer(player),checkersBoard, !myPlayer );
+                if(result <minUtility){
+                    minUtility = result;
+                }
+            }
+            System.out.println("MIN UTILITY"+minUtility);
+            return minUtility;
+        }
+    }
 }
--- src/main/java/checkers/bot/rivas/RivasBot.java
+++ src/main/java/checkers/bot/rivas/RivasBot.java
@@ -4,112 +4,31 @@ import checkers.CheckersBoard;
 import checkers.CheckersMove;
 import checkers.CheckersPlayer;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+
 import java.util.List;
-import java.util.Map;
 
 public class RivasBot implements CheckersPlayer {
-    private static final int LEVEL =5;
-    private static final boolean MYPLAYER = true; //true is black
-    List<CheckersMove> children ;
+    private static final boolean PLAYER = true; //true is black
+    private static final int LEVEL =10;
 
     @Override
-    public CheckersMove play(CheckersBoard board) {
+    public CheckersMove play(CheckersBoard board)  {
 
         List<CheckersMove> possibleCaptures = board.possibleCaptures();
         if (possibleCaptures.isEmpty()) {
-            bestMove(board);
             return getBuild(board);
         }
         return getBuild(board);
+
     }
 
-    private CheckersMove getBuild(CheckersBoard board) {
+    private CheckersMove getBuild(CheckersBoard board)
+    {
+        CheckersPablo check = new CheckersPablo();
+        CheckersMove move = check.getBestMove(board, PLAYER,LEVEL);
         return CheckersMove.builder()
-                .fromPosition(getBestMove(board, MYPLAYER).getStartRow(), getBestMove(board, MYPLAYER).getStartCol())
-                .toPosition(getBestMove(board, MYPLAYER).getEndRow(), getBestMove(board, MYPLAYER).getEndCol())
+                .fromPosition(move.getStartRow(), move.getStartCol())
+                .toPosition(move.getEndRow(), move.getEndCol())
                 .build();
     }
-
-    private void bestMove(CheckersBoard board){
-
-        System.out.println("Mi movimiento será de :"+ getBestMove(board, MYPLAYER).getStartRow() +" y: "+getBestMove(board, MYPLAYER).getStartCol());
-        System.out.println("A: ");
-        System.out.println(getBestMove(board, MYPLAYER).getEndRow()+ " y: "+getBestMove(board, MYPLAYER).getEndCol());
-
-    }
-    protected CheckersMove getBestMove(CheckersBoard board, boolean myPlayer)  {
-        if (RivasBot.LEVEL <1){
-            System.out.println("Level must be bigger than 0");
-        }
-        int maxUtility = -Integer.MAX_VALUE;
-        generateSuccessors(board);
-        CheckersMove bestMove = null;
-        CheckersBoard.Player currentPlayer;
-        currentPlayer = CheckersBoard.initBoard().getCurrentPlayer();
-        return getCheckersMove(board, maxUtility, bestMove, currentPlayer,myPlayer, RivasBot.LEVEL);
-    }
-
-    private CheckersMove getCheckersMove(CheckersBoard board, int maxUtility, CheckersMove bestMove, CheckersBoard.Player player, boolean myPlayer,int level) {
-        if (children == null){
-            return  null;
-        }
-        for (CheckersMove child : children) {
-            int utility = getUtility(level - 1, ch.otherPlayer(player), board, !myPlayer);
-            if (utility > maxUtility) {
-                maxUtility = utility;
-                bestMove = child;
-            }
-        }
-        return bestMove;
-    }
-    private int getUtility(int level, CheckersBoard.Player player, CheckersBoard board, boolean myPlayer) {
-        if (children.isEmpty()){
-            return heuristic();
-        }
-        if (level == 0) {
-            return heuristic();
-        }
-        generateSuccessors(board);
-        Map<CheckersMove, Integer> utilityMap = new HashMap<>();
-        for (CheckersMove successor: children) {
-            utilityMap.put(successor, getUtility(level-1,ch.otherPlayer(player), board, !myPlayer));
-        }
-        // it is my turn, so i am going to pick the best choice
-        if (myPlayer) {
-            int maxUtility = Integer.MIN_VALUE;
-            for (Map.Entry<CheckersMove, Integer> utilityEntry: utilityMap.entrySet()) {
-                if (utilityEntry.getValue() > maxUtility) {
-                    maxUtility = utilityEntry.getValue();
-                }
-            }
-
-            return maxUtility;
-        }else {
-            int minUtility = Integer.MAX_VALUE; // it is a kind of cheat
-            for (Map.Entry<CheckersMove, Integer> utilityEntry : utilityMap.entrySet()) {
-                if (utilityEntry.getValue() < minUtility) {
-                    minUtility = utilityEntry.getValue();
-
-                }
-            }
-            return minUtility;
-        }
-    }
-    private void generateSuccessors(CheckersBoard board) {
-        List<CheckersMove> successors = new ArrayList<>(100);
-        List<CheckersMove> possibleCaptures = board.possibleCaptures();
-        if (possibleCaptures.isEmpty()) {
-            List<CheckersMove> possibleMoves = board.possibleMoves();
-            successors.addAll(possibleMoves);
-        }else{
-            successors.addAll(possibleCaptures);
-        }
-        this.children = successors;
-    }
-
-
-
-
 }
