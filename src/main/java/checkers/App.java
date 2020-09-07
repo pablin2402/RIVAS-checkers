59b9a29ab8e1c8712068f0d1e18d4879e0b6b78c
pablin2402 <pabloperedo04@gmail.com>
2020-09-07 14:39:34 -0400

    last changes

--- src/main/java/checkers/App.java
+++ src/main/java/checkers/App.java
@@ -12,11 +12,11 @@ public class App {
 
     public static void main(String[] args) {
         CheckersBoard game = CheckersBoard.initBoard();
-        CheckersPlayer player1 = new GrayRandomBot();
-        CheckersPlayer player2 = new RivasBot();
-
+        CheckersPlayer player1 = new KeyboardPlayer();
+        CheckersPlayer player2 = new GrayRandomBot();
         Optional<CheckersPlayer> loser = game.play(player1, player2);
         loser.ifPresent(//
                 checkersPlayer -> System.out.println("LOSER! " + checkersPlayer.getClass().getName()));
     }
+
 }
